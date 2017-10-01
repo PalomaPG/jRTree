06bc0c4ed617c72f22059567205e3f0a5f88b16b
PalomaPG <paloma.perez.gar@gmail.com>
2017-10-01 12:48:27 -0300

    Insert method

---
+++ src/main/jRtree/exception/RTreeException.java
@@ -0,0 +1,13 @@
+package exception;
+
+import java.lang.Exception;
+
+public class RTreeException extends Exception{
+
+    public RTreeException(){}
+
+    public RTreeException(String msg){
+        super(msg);
+    }
+
+}
